a435bc67c7532c2db9605c266ba6cd7a26a0df1b
santi <sa_albarracin@hotmail.com>
2023-02-27 20:28:27 -0500

    testing the directory

---
+++ TowerDefense/src/main/java/com/gaming/Game.java
@@ -0,0 +1,11 @@
+package com.gaming;
+
+public class Game {
+
+    public Game(){
+        System.out.println("Testing");
+    }
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
